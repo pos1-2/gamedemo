2c110b5fcf0f817ae0ccf9741ab6980128e215f1
Florian Hassanen <florian.hassanen@gmail.com>
2016-03-01 13:59:24 +0100

    measeure and calculate time, extract constants

--- src/main/java/gamedemo/ConsoleMain.java
+++ src/main/java/gamedemo/ConsoleMain.java
@@ -2,17 +2,25 @@ package gamedemo;
 
 public class ConsoleMain {
 
-	private static final int UPS = 30;
+	private static final double HEIGHT = 1000.; // in meters
+	private static final double G = 10.; // free fall acceleration
+	private static final int UPS = 30; // updates per second
 	private static final long RATE = Math.round(1000000000.0/UPS);
+	
 	public static void main(String[] args) {
-		Stone s = new Stone(1000, -10.0/UPS/UPS);
+		Stone s = new Stone(HEIGHT, -G/UPS/UPS);
 
+		System.out.println("dropping the stone from " + HEIGHT + " meters...");
+		
 		long nextUpdate =  System.nanoTime() + RATE;
+		
+		long updateCount = 0;
 		
 		while(true) {
 			long t = System.nanoTime();
 			
 			if(nextUpdate < t) {
+				updateCount++;
 				s.update();
 				nextUpdate += RATE;
 			} else {
@@ -20,9 +28,12 @@ public class ConsoleMain {
 			}
 			
 			if(s.isBottom()) {
-				return;
+				break;
 			}
 		}
+		
+		System.out.println("simulation took " + (updateCount * (RATE / 1000000000.)) + " seconds");
+		System.out.println("exact calculation is " + Math.sqrt(2. * HEIGHT / G) + " seconds");
 	}
 
 }
