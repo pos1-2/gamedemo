4d11e896bf29ab8f139b80ed1d32ecf5bd377052
Florian Hassanen <florian.hassanen@gmail.com>
2016-03-01 09:54:52 +0100

    added stone model

---
+++ src/main/java/gamedemo/Stone.java
@@ -0,0 +1,25 @@
+package gamedemo;
+
+public class Stone {
+	private double height;
+	private double vel;
+	private double accel;
+	
+	public Stone(double height, double accel) {
+		this.height = height;
+		this.accel = accel;
+		this.vel = 0;
+	}
+	
+	public boolean isBottom() {
+		return this.height == 0;
+	}
+	
+	public void update() {
+		vel += accel;
+		height += vel;
+		if(height < 0) {
+			height = 0;
+		}
+	}
+}
